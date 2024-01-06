794d9591b087258c705c1e08464535432ddbb097
tillTheEndDeveloper <esulupov@gmail.com>
2024-01-06 17:50:33 +0200

    decomposition, hooks, custom hooks, pagination, work with fake rest api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useMemo, useRef} from "react";
+import {useState} from "react";
+import './styles/App.css'
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/modal/myModal";
+import MyButton from "./components/UI/button/MyButton";
+import {usePosts, useSortedPosts} from "./hooks/usePost";
+import axios, {post} from "axios";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/loader/Loader";
+import {useFetching} from "./hooks/useFetching";
+import {getPagesArray, getPagesCount} from "./utils/pages";
+import myButton from "./components/UI/button/MyButton";
+import Pagination from "./components/UI/pagination/Pagination";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([])
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false)
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+    const [totalPages, setTotalPages] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
+
+
+
+    const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+        const response = await PostService.getAll(limit, page)
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPagesCount(totalCount, limit))
+    })
+
+    useEffect(() => {
+        fetchPosts()
+    }, [page])
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+        setModal(false)
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+
+    const changePage = (page) =>{
+        setPage(page)
+    }
+
+
+    /* получаем данные из неуправляемого инпута
+    получаем доступ к ДОМ элементу
+    */
+
+    const bodyInputRef = useRef();
+
+
+    return (
+        <div className="App">
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+                Add post
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
+            <hr style={{margin: '15px 0'}}/>
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}
+            />
+
+            {postError &&
+                <h1>An error has occurred ${postError}</h1>
+            }
+
+            {isPostsLoading
+                ?
+                <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
+                    <Loader/>
+                </div>
+                : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Post List JS"/>
+            }
+            <Pagination page={page}
+                        changePage={changePage}
+                        totalPages={totalPages}
+            />
+        </div>
+    );
 }
 
 export default App;
